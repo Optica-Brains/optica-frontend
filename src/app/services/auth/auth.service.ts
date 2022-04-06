937bd34c7e9faf85b3c767c0bc66b01c62171192
james-muriithi <muriithijames556@gmail.com>
2022-04-06 03:56:51 +0300

    added services

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,7 +1,9 @@
+import { Router } from '@angular/router';
+import { TokenObject } from './../../models/token-object.model';
 import { User } from './../../models/user.model';
 import { Injectable } from '@angular/core';
-import { Observable, retry } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { catchError, map, Observable, retry, throwError } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 import { environment } from './../../../environments/environment';
 
@@ -11,11 +13,43 @@ import { environment } from './../../../environments/environment';
 export class AuthService {
 
   public user!: Observable<User>;
+  public userTokenObject!: Observable<TokenObject>;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router:Router) { }
 
-  public login(email: String, password: String): Observable<any> {
-    return this.http.post<void>(`${environment.apiUrl}/api/login`, { email, password }).pipe(retry(1))
+  public login(email: string, password: string): Observable<any> {
+    return this.http.post<void>(`${environment.apiUrl}/api/login/`, { email, password }, {withCredentials:  true})
+      .pipe(map(token => {
+        localStorage.setItem('token', JSON.stringify(token))
+        return token
+      })).pipe(catchError(error => {
+        let errorMsg: string = '';
+        if (error.error instanceof ErrorEvent) {
+            errorMsg = `There was an error logging you in`;
+        } else if (error instanceof HttpErrorResponse) {
+            errorMsg = "You provided incorrect login credentials";
+        }
+
+        return throwError(errorMsg);
+    }))
+  }
+
+  logout(): void {
+    localStorage.removeItem("user");
+    localStorage.removeItem("token");
+    this.router.navigate(["/login"]);
+  }
+
+  getLoggedUserToken():TokenObject{
+    return JSON.parse(localStorage.getItem("token") ?? '');
+  }
+
+  getLoggedUserDetails(): User {
+    return JSON.parse(localStorage.getItem("user") ?? '');
+  }
+
+  isUserAuthenticated(): boolean {
+    return !!localStorage.getItem("token");
   }
 
 }
