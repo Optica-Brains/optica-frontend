9e0a5b642e0d96fdefd122a59013f29cdab152fb
Marcos8060 <marcosgav80@gmail.com>
2022-04-12 22:41:56 +0300

    made minor changes in the users component

--- src/app/pages/components/users/users.component.ts
+++ src/app/pages/components/users/users.component.ts
@@ -1,4 +1,8 @@
+import { Role } from './../../../models/role.model';
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-users',
@@ -7,9 +11,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+  public userId: any;
+
+  public fullname!: any;
+  public email!: any;
+  public password!: string;
+  public role!: string;
+
+  public Role = Role
+
+
+
+  constructor( private http : HttpClient) {}
+
+  users:any
+
 
   ngOnInit(): void {
+    // get all users
+    this.users = this.http.get('http://127.0.0.1:8000/api/users/').subscribe(
+      data => this.users = data      
+    )   
   }
 
+  userGroups(groups:[]){
+    return groups.map((group)=> this.Role[group]).join(' , ')
+  }
+
+  createUser(){
+      var data = {
+        full_name : this.fullname,
+        email : this.email,
+        password : this.password,
+        groups : [this.role]
+      }
+      this.http.post("http://127.0.0.1:8000/api/users/", data).subscribe((res) => console.log(res)); 
+  }
 }
